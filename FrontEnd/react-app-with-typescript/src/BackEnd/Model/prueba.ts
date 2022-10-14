d490b53c70b6eaca5d42c672ccf2a7eb022a3320
J-BS-PNG <josuebarrientossandoval@gmail.com>
2022-10-14 00:18:34 -0600

    Cambios
    Objeto area se le agrego atributo floor y se creo un enum Floor.

--- FrontEnd/react-app-with-typescript/src/BackEnd/Model/prueba.ts
+++ FrontEnd/react-app-with-typescript/src/BackEnd/Model/prueba.ts
@@ -1,8 +1,8 @@
 import {Area} from './Area'
 import {Element} from './Element'
 import { Inspection, InspectionArea, InspectionElement, FactoryInspections, State} from './Inspection'
-let area1 = new Area('dsd', 'dsda', ['dsdsa'], 'dasda');
-let element = new Element('dasd', 'dada', ['dad'], 'dadasd', area1);
+//let area1 = new Area('dsd', 'dsda', ['dsdsa'], 'dasda');
+let element = new Element('dasd', 'dada', ['dad'], 'dadasd', null!);
 let factoryInspection: FactoryInspections = new FactoryInspections();
 let pArea: Inspection = factoryInspection.getInspection(2, 2333,'hola mundo', null!, null!,null!,null!,'txt.pdf', State.Ejecutada, element);
 
