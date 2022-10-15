1cc34783461638c96a738ebbb868c0dc9cf5f1cf
IssaAC00 <algueracmp@gmail.com>
2022-10-15 03:05:51 -0600

    Tareas Encargados
    Falta tabla

--- FrontEnd/react-app-with-typescript/src/Pantallas/Encargado.tsx
+++ FrontEnd/react-app-with-typescript/src/Pantallas/Encargado.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 import  Navbar  from "../componentes/topbar"
 
 
+
+
 function Encargado (): JSX.Element {
+
+
+        const [selectedOption, setSelectedOption] = useState<String>();
+
+        const radioHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+                setSelectedOption(event.target.value);
+                console.log(selectedOption);
+              };
+              
 
         return (
 
             <div> 
                 <Navbar />
-                Hola soy encargado</div>
+                
+                   <input
+                        type="radio"
+                        name="drink"
+                        value="Coffee"
+                        id="coffee"
+                        onChange={radioHandler}
+                        />
+                <label htmlFor="coffee">Coffee</label>
+               
+               
+               
+               
+               
+               </div>
 
 
         )
