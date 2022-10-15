019d0bcbd5553296da80837d50fb6847cd4e4591
IssaAC00 <algueracmp@gmail.com>
2022-10-15 03:09:05 -0600

    Update Contraseña.tsx

--- FrontEnd/react-app-with-typescript/src/Pantallas/Contraseña.tsx
+++ FrontEnd/react-app-with-typescript/src/Pantallas/Contraseña.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import '../componentes/dropdownS.css'
+import  Navbar  from "../componentes/topbar"
+import '../componentes/inputEstiloGlobal.css'
+import React, { useState } from "react";
+import "./areas.css"
 import '../componentes/buttonS.css'
-import {controller} from '../BackEnd/Controller/Controller'
+
 
 
 function Contraseña (): JSX.Element{
 
-        function Ingresar(){
-            controller.seePolaige('1');
-        }
+        
         return (
 
 
-            <div> Hola soy contraseña
+            <div>
+                
 
-                <button onClick={Ingresar} className='buttonS' style = {{position: 'absolute', top: 710, left: 700, fontSize: 23, fontWeight: 'bold'}}>Prueba</button>
+            <label style = {{position: 'absolute', top: 200, left: 300, fontSize: 32, fontWeight: 'bold'}}> Correo  </label>
+            <input type="text"  className='input-global'  style = {{position: 'absolute', top: 150, left: 500, fontSize: 32}} />
+
+            <label style = {{position: 'absolute', top: 400, left: 300, fontSize: 32, fontWeight: 'bold'}}> Nueva Contraseña </label>
+            <input type="text"  className='input-global' style = {{position: 'absolute', top: 350, left: 600, fontSize: 23, fontWeight: 'bold'}} />
+
+            <button  className='buttonS' style = {{position: 'absolute', top: 700, left: 100, fontSize: 23}}>Volver</button>
+              
             </div>
             
         )
