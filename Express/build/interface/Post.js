9b992c26cba210478adf0de668db11e9c7f3e530
IssaAC00 <algueracmp@gmail.com>
2022-10-21 14:30:15 -0600

    Revert "Express"
    This reverts commit 8a756d88abafab8e9f5390bd5010b5f627ec917c.

--- Express/build/interface/Post.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* CREATE TABLE USUARIO (
-    userMail VARCHAR(150) PRIMARY KEY,
-    userPassword VARCHAR(150),
-    rol integer,
-    FOREIGN KEY (rol) REFERENCES Rol (id)
-);
- */ 
