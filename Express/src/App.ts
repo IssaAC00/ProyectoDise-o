d876b788554b1c187c2f2d040819bac67d4cf486
IssaAC00 <algueracmp@gmail.com>
2022-10-27 21:33:17 -0600

    Update App.ts

--- Express/src/App.ts
+++ Express/src/App.ts
@@ -1,19 +1,8 @@
 import express, { Application } from 'express'
 
 
-import Area from './Routes/Area'
-import dutyManager from './Routes/DutyManager'
-import estado from './Routes/estado'
-import floortype from './Routes/floortype'
-import inspeccion from './Routes/inspeccion'
-import inspeccionArea from './Routes/inspeccionArea'
-import inspeccionElemento from './Routes/inspeccionElemento'
-import rol from './Routes/rol'
-import spolaigeArea from './Routes/spolaigeArea'
-import typeSpolaige from './Routes/typeSpolaige'
-import typeWork from './Routes/typeWork'
 import usuario from './Routes/usuarios'
-import elemento from './Routes/Element'
+
 
 
 
@@ -41,18 +30,7 @@ export class App {
 
     
     private routes() {
-        this.app.use('/area', Area);
-        this.app.use('/dutymanager', dutyManager);
-        this.app.use('/elemento', elemento);
-        this.app.use('/estado', estado);
-        this.app.use('/floortype', floortype);
-        this.app.use('/inspeccion', inspeccion);
-        this.app.use('/inspeccionArea', inspeccionArea);
-        this.app.use('/inspeccionElemento', inspeccionElemento);
-        this.app.use('/rol', rol);
-        this.app.use('/spolaigeArea', spolaigeArea);
-        this.app.use('/typeSpolaige', typeSpolaige);
-        this.app.use('/typeWork', typeWork);
+      
         this.app.use('/usuarios', usuario);
     }
 
